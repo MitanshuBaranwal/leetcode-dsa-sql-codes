566fc7ad927b6cdd5536d1e6d763202afb72f43d
Mitanshu Baranwal <67519721+MitanshuBaranwal@users.noreply.github.com>
2025-07-13 23:01:34 +0530

    Time: 1121 ms (44.23%) | Memory: 13.3 MB (48.71%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        diff= 0
+        dct= {nums[i]:i for i in range(len(nums))}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            print(diff)
+            if diff in dct.keys() and dct[diff]!=i:
+                return [i,dct[diff]]
+        return False
