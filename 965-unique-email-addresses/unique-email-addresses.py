7f6099843d57fcafe491d2904efbe0f0c01fb2d7
Mitanshu Baranwal <67519721+MitanshuBaranwal@users.noreply.github.com>
2025-07-15 22:57:32 +0530

    Time: 5 ms (56.37%) | Memory: 18 MB (25.28%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -1,5 +1,6 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
+        st = set()
         for i in range(len(emails)):
             mailid, domain=  emails[i].split("@")
             if "." in  mailid:
@@ -7,5 +8,5 @@ class Solution:
             if "+" in mailid:
                 mailid= mailid.split("+")[0]
             finalmail = mailid+"@"+domain
-            emails[i] = finalmail
-        return len(set(emails))
+            st.add(finalmail)
+        return len(st)
