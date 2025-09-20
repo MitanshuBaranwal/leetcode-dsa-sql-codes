88001c5dddd66d5e8cb7c07d8acbf06524703682
Mitanshu Baranwal <67519721+MitanshuBaranwal@users.noreply.github.com>
2025-09-20 15:52:46 +0530

    Time: 7 ms (55.98%) | Memory: 19.4 MB (18.84%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dct= {}
-        length = len(nums)//2
-        for i,j in enumerate(nums):
-            print(i,j)
-            dct[j] = dct.get(j,0)+1
-            if dct[j] >length:
-                return j
+        dct= Counter(nums)
+        length= len(nums)//2
+        for i in dct.keys():
+            if dct[i] > length:
+                return i
