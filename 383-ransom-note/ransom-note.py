69a47ca745c1672782a26832b9651ea51e41fdce
Mitanshu Baranwal <67519721+MitanshuBaranwal@users.noreply.github.com>
2025-09-25 23:54:04 +0530

    Time: 15 ms (75.37%) | Memory: 17.8 MB (96.67%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -3,9 +3,6 @@ class Solution:
         ransomNotedct= Counter(ransomNote)
         magazinedct= Counter(magazine)
         for i in ransomNotedct:
-            print(i)
             if i not in magazinedct or ransomNotedct[i] > magazinedct[i]:
-                print(ransomNotedct[i])
-                print(magazinedct[i])
                 return False
         return True
