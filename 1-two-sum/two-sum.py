09271f4902e2e979108abc9087f595eae8228990
Mitanshu Baranwal <67519721+MitanshuBaranwal@users.noreply.github.com>
2025-10-07 08:39:33 +0530

    Time: 542 ms (45.88%) | Memory: 13 MB (90.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         dct = dict()
         for i,j in enumerate(nums):
-            diff = target-j
-            if dct.keys() and diff in dct.keys():
+            diff = target - j
+            if diff in dct.keys():
                 return [dct[diff],i]
             dct[j] = i
-
